7e656ad1ed63837c5df2c06b3502b2ff96b4aa2d
Corey M <corey.mason98@googlemail.com>
2014-01-30 16:57:02 -0800

    Start of Autonomous

--- src/org/usfirst/frc3482/Awesome/commands/Autonomous.java
+++ src/org/usfirst/frc3482/Awesome/commands/Autonomous.java
@@ -1,18 +1,23 @@
 package org.usfirst.frc3482.Awesome.commands;
 
-import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.CommandGroup;
 import org.usfirst.frc3482.Awesome.Robot;
 
-public class  Autonomous extends Command {
-
+public class  Autonomous extends CommandGroup {
+    
+    private boolean isFinished = false;
     public Autonomous() {
         // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
 	requires(Robot.camera);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+        addParallel(new PullBack());
+        addSequential(new Aim());
+        addSequential(new Shoot());
+        addSequential(new Move(2,0,5));
+        isFinished = true;
     }
 
     // Called repeatedly when this Command is scheduled to run
@@ -21,7 +26,7 @@ public class  Autonomous extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return isFinished;
     }
 
     // Called once after isFinished returns true
@@ -31,5 +36,6 @@ public class  Autonomous extends Command {
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+        end();
     }
 }
